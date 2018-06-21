d4edce71b65201dd515dbb0603035a7e8ac46e89
DesarrolloAccutone <desarrollo@accutone.com.mx>
2018-06-21 11:54:18 -0500

    botman-charla-I

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use BotMan\BotMan\BotMan;
 use Illuminate\Http\Request;
 use App\Conversations\ExampleConversation;
+use App\Conversations\charla;
 
 class BotManController extends Controller
 {

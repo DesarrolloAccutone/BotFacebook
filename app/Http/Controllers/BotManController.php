bfe6971b7ca2a36a5e1c0a36b2194c06d076f0dc
DesarrolloAccutone <desarrollo@accutone.com.mx>
2018-06-21 11:34:34 -0500

    botman-charlaI

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -34,4 +34,9 @@ class BotManController extends Controller
     {
         $bot->startConversation(new ExampleConversation());
     }
+
+    public function iniciarPlatica(BotMan $bot)
+    {
+        $bot->iniciarPlatica(new charla());
+    }
 }

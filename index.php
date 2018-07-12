8ef07b02dc63bf5047938b4a110a8c8dc20b4aa7
DesarrolloAccutone <desarrollo@accutone.com.mx>
2018-07-12 10:03:04 -0500

    botmanF clear

--- index.php
+++ index.php
@@ -1,55 +1,5 @@
 <?php
 
-/*require('vendor/autoload.php');
-
-use BotMan\BotMan\BotMan;
-use BotMan\BotMan\BotManFactory;
-use BotMan\BotMan\Drivers\DriverManager;
-use Botman\Botman\Facebook\ElementButton;
-use Botman\Botman\Facebook\ButtonTemplate;
-
-$config = [
-	'facebook' => [
-	  	'token' => 'EAADQLGhaeSkBAC7FOgeyjVL6X1mexDlo12G9TXGNMFckJx375eTtlVIyaQAEdsxpjXVpjNXiZCGEe3tjfuNdm3Qa7abbvSaPEDsG8VDibdqJezeCAaaND5uYWX0w4cpWY03z2SuIfL90KZAM0WQyiZCfvyTobvgfSjZA1Kip1wZDZD',
-		'app_secret' => '741f3b8f86eea022e6861534e15f7704',
-	    'verification'=>'botmanACC',
-	]
-
-];
-
-// Load the driver(s) you want to use
-DriverManager::loadDriver(\BotMan\Drivers\Facebook\FacebookDriver::class);
-
-// Create an instance
-
-$botman = BotManFactory::create($config);
-
-// Give the bot something to listen for.
-$botman->hears('Hola', function (BotMan $bot) {
-    $bot->typesAndWaits(2);
-    $bot->reply('Hola soy AccuBot');
-    $bot->typesAndWaits(2);
-    $bot->reply('¿Como te llamas?');
-});
-
-$botman->hears('Me llamo {name}', function ($bot, $name) {
-	$bot->typesAndWaits(2);
-	$bot->reply('¿En que te puedo ayudar '.$name.' ?');
-});
-
-$botman->hears('Button', function (Botman $bot) {
-    $bot->reply('¿Button?');
-    $bot->reply(ButtonTemplate::create('Do you want to know more about BotMan?')
-		->addButton(ElementButton::create('Tell me more')->type('postback')->payload('tellmemore'))
-		->addButton(ElementButton::create('Show me the docs')->url('http://botman.io/'))
-	);
-	$bot->reply('¿Button2?');
-    
-});
-
-
-// Start listening
-$botman->listen();*/
 
 require('vendor/autoload.php');
 
